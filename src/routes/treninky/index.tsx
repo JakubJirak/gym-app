33a2767348e7f8cffffff37673df233b1e61c868
Jakub Jirák <jirak.jakub12@seznam.cz>
2025-07-16 00:40:19 +0200

    konecne oprava tailwind animaci u accordionu, pridani defaultniho accordionu z origin ui do treninku pro zobrazovani treninku

--- src/routes/treninky/index.tsx
+++ src/routes/treninky/index.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header.tsx";
 import AddTraining from "@/components/treninky/AddTraining";
-import { createFileRoute } from "@tanstack/react-router";
+import Training from "@/components/treninky/Training.tsx";
+import {createFileRoute} from "@tanstack/react-router";
 
 export const Route = createFileRoute("/treninky/")({
   component: RouteComponent,
@@ -11,6 +12,9 @@ function RouteComponent() {
     <div>
       <Header page="TRÉNINKY" />
       <AddTraining />
+      <div className="w-[90%] max-w-[500px] mx-auto gap-4 flex flex-col justify-center mt-4">
+        <Training />
+      </div>
     </div>
   );
 }
