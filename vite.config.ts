0e2e8aedf095e291ca8046e5a16b89a933db9e13
Jakub Jirák <jirak.jakub12@seznam.cz>
2025-07-23 15:56:25 +0200

    fix: update bundler

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ const config = defineConfig({
         }),
         tailwindcss(),
         tanstackStart({
+            target: "bun",
             customViteReactPlugin: true,
         }),
         react(),
