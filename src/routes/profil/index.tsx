57503d6844655bddf08522154921296df96372dd
Jakub Jirák <jirak.jakub12@seznam.cz>
2025-07-24 13:47:30 +0200

    pridani stranky profil

--- src/routes/profil/index.tsx
+++ src/routes/profil/index.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/Header.tsx";
+import { Card, CardContent } from "@/components/ui/card.tsx";
+import { Separator } from "@/components/ui/separator.tsx";
 import { authClient } from "@/lib/auth-client.ts";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
@@ -20,6 +22,28 @@ function RouteComponent() {
   return (
     <div>
       <Header page="PROFIL" />
+      <div className="max-w-[500px] w-[90%] mx-auto">
+        <Card className="p-4">
+          <CardContent className="px-2">
+            <div className="space-y-2">
+              <div>
+                <p className="text-muted-foreground">Uživatelské jméno</p>
+                <p>{session.user.name}</p>
+              </div>
+              <Separator />
+              <div>
+                <p className="text-muted-foreground">Email</p>
+                <p>{session.user.email}</p>
+              </div>
+              <Separator />
+              <div>
+                <p className="text-muted-foreground">Založení účtu</p>
+                <p>{session.user.createdAt.toLocaleDateString()}</p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
