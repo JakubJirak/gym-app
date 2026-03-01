c074fbdde250b2f55753090f6ea40f85e9669c23
Jakub Jirak <jirak.jakub12@seznam.cz>
2026-03-01 10:25:49 +0100

    feat: odstraneni textu v herosekci a nahrazeni odkazem na novou verzi aplikace

--- src/components/index/HeroSection.tsx
+++ src/components/index/HeroSection.tsx
@@ -3,15 +3,13 @@ import { Link } from "@tanstack/react-router";
 
 const HeroSection = () => {
   return (
-    <section className="h-[100dvh] flex flex-col items-center">
-      <h1 className="text-[5.3dvh]/[6.7dvh] text-left w-[80%] mt-[13dvh] mx-auto font-bold">
-        Získejte kontrolu nad svým tréninkem
+    <section className="pb-20 flex flex-col items-center">
+      <h1 className="text-3xl text-left w-[80%] mt-[13dvh] mx-auto font-bold">
+        Toto je stará verze aplikace GYM TRACKER.
       </h1>
-      <p className="tracking-wider font-light text-[2.8dvh]/[4.7dvh] text-left w-[80%] mx-auto mt-[4dvh]">
-        Jednoduše si zapisujte tréninky a sledujte svůj progres v přehledných
-        statistikách a souhrnech. Profesionální nástroj pro každého, kdo to s
-        pohybem myslí vážně.
-      </p>
+      <a href="https://gymtracker.jirak.dev/" className="mt-8 text-2xl  text-primary underline">
+        Nová verze aplikace je dostupná zde
+      </a>
       <div className="flex flex-col gap-[2dvh] w-[80%] mx-auto mt-[6dvh] justify-between">
         <Link to={"/login"} className="inline-flex">
           <Button size="xl" className="cursor-pointer">
